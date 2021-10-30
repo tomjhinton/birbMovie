899162d684edfe9cfc682308e36a1625626bfbb3
Tom Hinton <tomjhinton@gmail.com>
2021-10-30 20:31:42 +0100

    deploy

---
+++ src/app.js
@@ -0,0 +1,237 @@
+import './style.scss'
+import * as THREE from 'three'
+
+import { gsap } from 'gsap'
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+import * as CANNON from 'cannon-es'
+import cannonDebugger from 'cannon-es-debugger'
+
+
+import vertexShaderScreen from './shaders/screen/vertex.glsl'
+
+import fragmentShaderScreen from './shaders/screen/fragment.glsl'
+
+import vertexShaderHonk from './shaders/honk/vertex.glsl'
+
+import fragmentShaderHonk from './shaders/honk/fragment.glsl'
+
+
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+
+const gtlfLoader = new GLTFLoader()
+
+
+const geometryScreen = new THREE.PlaneGeometry( 2, 2, 128, 128)
+
+
+// Materials
+const materialScreen = new THREE.ShaderMaterial({
+  vertexShader: vertexShaderScreen,
+  fragmentShader: fragmentShaderScreen,
+  transparent: true,
+  depthWrite: true,
+  clipShadows: true,
+  wireframe: false,
+  side: THREE.DoubleSide,
+  uniforms: {
+    uTime: {
+      value: 0
+    },
+    uMouse: {
+      value: {x: 0.5, y: 0.5}
+    },
+    uResolution: { type: 'v2', value: new THREE.Vector2() },
+    uPosition: {
+      value: {
+        x: 0
+      }
+    }
+  }
+})
+
+const materialText = new THREE.ShaderMaterial({
+  vertexShader: vertexShaderHonk,
+  fragmentShader: fragmentShaderHonk,
+  transparent: true,
+  depthWrite: true,
+  clipShadows: true,
+  wireframe: false,
+  side: THREE.DoubleSide,
+  uniforms: {
+    uTime: {
+      value: 0
+    },
+    uMouse: {
+      value: {x: 0.5, y: 0.5}
+    },
+    uResolution: { type: 'v2', value: new THREE.Vector2() },
+    uPosition: {
+      value: {
+        x: 0
+      }
+    }
+  }
+})
+
+window.addEventListener('mousemove', function (e) {
+  materialScreen.uniforms.uMouse.value.x =  (e.clientX / window.innerWidth) * 2 - 1
+  materialScreen.uniforms.uMouse.value.y = -(event.clientY / window.innerHeight) * 2 + 1
+  console.log(materialScreen.uniforms.uMouse)
+})
+
+
+
+
+const screenMesh = new THREE.Mesh(geometryScreen, materialScreen)
+
+
+
+scene.add(screenMesh)
+
+
+
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>{
+
+
+
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2 ))
+
+  materialScreen.uniforms.uResolution.value.x = renderer.domElement.width
+  materialScreen.uniforms.uResolution.value.y = renderer.domElement.height
+
+
+})
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+scene.add(camera)
+
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  alpha: true
+})
+
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.setClearColor( 0x000000, 1 )
+
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
+
+let textMesh
+var loader = new THREE.FontLoader()
+loader.load( 'Tapeworm_RegularH.json', function ( font ) {
+
+  var textGeometry = new THREE.TextGeometry( 'Birbs go honk,  the movie', {
+
+    font: font,
+
+    size: 1,
+    height: 1,
+    curveSegments: 0,
+
+    bevelThickness: 1.,
+    bevelSize: 1.,
+    bevelEnabled: false
+
+  })
+
+
+
+  textMesh = new THREE.Mesh( textGeometry, materialText )
+
+  scene.add( textMesh )
+
+
+  textMesh.position.z-=15
+  textMesh.position.x-=8
+})
+
+let sceneGroup, mixer
+gtlfLoader.load(
+  'birds.glb',
+  (gltf) => {
+
+    gltf.scene.scale.set(1.5,1.5,1.5)
+    sceneGroup = gltf.scene
+    sceneGroup.needsUpdate = true
+    sceneGroup.position.z -= 17
+    sceneGroup.position.x += 1.6
+    scene.add(sceneGroup)
+
+    if(gltf.animations[0]){
+      mixer = new THREE.AnimationMixer(gltf.scene)
+
+      const action2 = mixer.clipAction(gltf.animations[1])
+
+      action2.play()
+
+    }
+
+
+  }
+)
+
+
+const light = new THREE.AmbientLight( 0x404040 )
+scene.add( light )
+
+
+
+const clock = new THREE.Clock()
+
+const tick = () =>{
+  if ( mixer ) mixer.update( clock.getDelta() )
+  const elapsedTime = clock.getElapsedTime()
+
+
+  if(materialScreen.uniforms.uResolution.value.x === 0 && materialScreen.uniforms.uResolution.value.y === 0 ){
+    materialScreen.uniforms.uResolution.value.x = renderer.domElement.width
+    materialScreen.uniforms.uResolution.value.y = renderer.domElement.height
+  }
+
+  materialScreen.uniforms.uTime.value = elapsedTime
+
+  materialText.uniforms.uTime.value = elapsedTime
+
+
+  renderer.render(scene, camera)
+
+
+  window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ src/shaders/screen/fragment.glsl
@@ -0,0 +1,131 @@
+
+const float PI = 3.1415926535897932384626433832795;
+const float TAU = PI * 2.;
+
+
+uniform vec2 uResolution;
+uniform vec2 uMouse;
+
+
+varying vec2 vUv;
+varying float vTime;
+
+
+
+float wiggly(float cx, float cy, float amplitude, float frequency, float spread){
+
+  float w = sin(cx * amplitude * frequency * PI) * cos(cy * amplitude * frequency * PI) * spread;
+
+  return w;
+}
+
+
+
+//	Classic Perlin 2D Noise
+//	by Stefan Gustavson
+//
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 *
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
+void coswarp(inout vec3 trip, float warpsScale ){
+
+  trip.xyz += warpsScale * .1 * cos(3. * trip.yzx + (vTime * .25));
+  trip.xyz += warpsScale * .05 * cos(11. * trip.yzx + (vTime * .25));
+  trip.xyz += warpsScale * .025 * cos(17. * trip.yzx + (vTime * .25));
+  // trip.xyz += warpsScale * .0125 * cos(21. * trip.yzx + (vTime * .25));
+}
+
+
+
+
+
+vec2 getRadialUv(vec2 uv) {
+	float angle = atan(uv.x, -uv.y);
+	angle = abs(angle);
+	vec2 radialUv = vec2(0.0);
+	radialUv.x = angle / (PI * 2.0) + 0.5;
+	radialUv.y = 1.0 - pow(1.0 - length(uv), 4.0);
+	return radialUv;
+}
+
+
+void uvRipple(inout vec2 uv, float intensity){
+
+	vec2 p =-1.+2.*gl_FragCoord.xy / uResolution.xy-vec2(0,-.001);
+
+
+    float cLength=length(p);
+
+     uv= uv +(p/cLength)*cos(cLength*15.0-vTime*1.0)*intensity;
+
+}
+
+float box(vec2 _st, vec2 _size, float _smoothEdges){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 aa = vec2(_smoothEdges*0.5);
+    vec2 uv = smoothstep(_size,_size+aa,_st);
+    uv *= smoothstep(_size,_size+aa,vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+
+void main(){
+  float alpha = 1.;
+    vec2 uv = (gl_FragCoord.xy - uResolution * .5) / uResolution.yy ;
+
+  float circle = step(distance(uv, uMouse), .3 + wiggly(vUv.x + vTime * .05, vUv.y + vTime * .05, 4., 2., 0.05));
+
+  vec3 color = vec3(uv.x, uv.y, 1.);
+
+  uvRipple(color.rg, .5);
+  coswarp(color, 3.);
+
+  color = mix(color, 1.-color, box(vUv, vec2(.95), .5));
+
+  color = mix(color, vec3(uv.y, uv.x, 1.), box(vUv, vec2(.5), .5));
+
+
+
+
+
+ gl_FragColor =  vec4(color, 1.-circle);
+
+}
